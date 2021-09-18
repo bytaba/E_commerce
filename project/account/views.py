ce4299cedfc8973f1d378d9c06eea7736a6eafcd
taba <tabatabaeipour.sa@gmail.com>
2021-09-18 18:43:39 +0430

    start a new app: account

---
+++ project/account/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def login_view (request):
+    return render(request , 'account/login.html' , {})
+
+
+
+def register_view (request):
+    return render(request , 'account/register.html' , {})
+
+
+
+def logout_view (request):
+    pass
